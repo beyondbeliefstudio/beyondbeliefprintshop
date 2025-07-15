658da17ee44832fc41d342cca5679314c136049f
Chris Nervegna <contact@beyondbeliefstudio.com>
2025-07-14 20:09:07 -0400

    lots of updates

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { glob, file } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const reviews = defineCollection({
-  loader: file("src/content/reviews.json"),
+  loader: file("src/data/reviews.json"),
   schema: ({ image }) =>
     z.object({
       name: z.string(),
@@ -14,7 +14,7 @@ const reviews = defineCollection({
 });
 
 const heroCards = defineCollection({
-  loader: file("src/content/heroCards.json"),
+  loader: file("src/data/heroCards.json"),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
@@ -25,4 +25,26 @@ const heroCards = defineCollection({
     }),
 });
 
-export const collections = { reviews, heroCards };
+const orderingProcess = defineCollection({
+  loader: file("src/data/orderingProcess.json"),
+  schema: ({ image }) =>
+    z.object({
+      data: z.string(),
+      title: z.string(),
+      description: z.string(),
+      image: image(),
+      imageAlt: z.string(),
+      active: z.string(),
+    }),
+});
+
+const servicesFAQ = defineCollection({
+  loader: file("src/data/servicesFAQ.json"),
+  schema: z.object({
+    question: z.string(),
+    answer: z.string(),
+    tag: z.string(),
+  }),
+});
+
+export const collections = { reviews, heroCards, orderingProcess, servicesFAQ };
